4c30dd8cc0052aa3208bf202f08e304865532c6b
Aldiahmad7 <aldiahmaddani0@gmail.com>
2025-01-31 14:29:30 +0700

    dashboard template ui

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('login');
+    return view('dashboard');
 });
+
+// Route::get('/', function () {
+//     return view('login');
+// });
