7072bef922d381ad4596f6c27bb43b277fdda8da
Aldiahmad7 <aldiahmaddani0@gmail.com>
2025-02-01 12:15:39 +0700

    ui login

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('login');
 });
-
-// Route::get('/', function () {
-//     return view('login');
-// });
